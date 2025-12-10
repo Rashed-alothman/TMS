4654c48597227f74c6b1315982f633cc683e73e2
Rashed Alothman <rashed.alothman@initialsaudi.local>
2025-12-10 10:58:08 +0300

    adding some app routing to make sure of the idea is clear for me if there any thing more the that i will commit (wow say wallh a don't what to say it can just say your to يبغ)

--- app.py
+++ app.py
@@ -3,9 +3,23 @@
 # Auther: Rashed Alothman.
 # I just want to say to myself i wish you the best.
 from flask import Flask
-
+from flask improt request
+import request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def helloWorld():
     return "<p> Hello World How the fuck are you!</p>"
+
+@app.rotue("/add_Tasks",methods=["GET","POST"])
+def AddTasks(int Date, str NameOfTheTask,str detileofthetask,):
+
+
+@app.route("/delete_task")
+def deleteTask(int ID)
+
+
+@app.route("/AddUsersToAccout")
+def addUsers(str email)
+
+@app.route("/about me")
