9141df024ce08479dc48798128af348cef556546
Rashed Alothman <rashed.alothman@initialsaudi.local>
2025-12-11 08:07:06 +0300

    update some of the routes and adding some futures

--- app.py
+++ app.py
@@ -2,24 +2,31 @@
 # Date: 10/12/2025--dd/mm/yyyy. 
 # Auther: Rashed Alothman.
 # I just want to say to myself i wish you the best.
-from flask import Flask
-from flask improt request
-import request
+from flask import Flask ,request
 app = Flask(__name__)
 
 @app.route("/home")
 def helloWorld():
     return "<p> Hello World How the fuck are you!</p>"
 
-@app.rotue("/add_Tasks",methods=["GET","POST"])
-def AddTasks(int Date, str NameOfTheTask,str detileofthetask,):
+@app.route("home/tasks/add_Tasks",methods = ["POST"])
+def AddTasks():
+    data = request.get_json()
+    descripton = data.get("description")
+    task ={
+        "id":len(tasks)+1,
+        "descripton":descripton
+    }
+    task.append(task)
+    return {"massage":"task addad","task":task},201
 
+@app.route("home/task/delete_task")
+def deleteTask(ID):
+    return 0
 
-@app.route("/delete_task")
-def deleteTask(int ID)
-
-
-@app.route("/AddUsersToAccout")
-def addUsers(str email)
-
-@app.route("/about me")
+@app.route("home/AddUsersToAccout")
+def addUsers(email):
+    return 0
+@app.route("home/User/about me")
+def aboutMe():
+    return 0
